ab87122fb68f71f583753063e2b3581ca444d0ce
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-03-27 07:40:16 -0300

    :books: docs: Modificações

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './App'
-import './index.css'
+import Header from './assets/components/Header'
+import Footer from './assets/components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header/>
     <Home />
+    <Footer/>
   </React.StrictMode>,
 )
